21d4e6816502c8d8def3b42a28e2700512c544c7
shyamalapeesapati <shyamalapeesapati@shyamalas-mbp-2.attlocal.net>
2021-10-21 22:53:26 -0700

    2 Commit - TC2 created

---
+++ HRMS/src/demos/Testcase2.java
@@ -0,0 +1,12 @@
+package demos;
+
+public class Testcase2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This is Testcase2");
+
+	}
+
+}
